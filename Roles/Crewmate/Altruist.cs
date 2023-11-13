2236be64236228c941bae44acbc8e012474bc3ef
Gurge44 <gurge44.minecraft@gmail.com>
2023-11-13 17:32:46 +0100

    Bruh I forgot to pull

---
+++ Roles/Crewmate/Altruist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOHE.Roles.Crewmate
+{
+    public static class Altruist
+    {
+        private static readonly int Id = 642600;
+    }
+}
