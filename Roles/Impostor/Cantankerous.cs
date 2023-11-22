12ee599bca7e4f330e9ba2588e2e9c1184bb5cdc
Gurge44 <gurge44.minecraft@gmail.com>
2023-11-22 20:47:13 +0100

    Just add these so I don't forget what these do

---
+++ Roles/Impostor/Cantankerous.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOHE.Roles.Impostor
+{
+    public static class Cantankerous
+    {
+        // points for non-imps ejected, require points for killing
+    }
+}
