8614486bb7927f9dec6617108d4e00d695029625
Gurge44 <gurge44.minecraft@gmail.com>
2024-04-06 20:38:36 +0200

    Code Cleanup

--- Roles/Neutral/NSerialKiller.cs
+++ Roles/Neutral/NSerialKiller.cs
@@ -1,5 +1,4 @@
 using AmongUs.GameOptions;
-using System.Collections.Generic;
 using static EHR.Options;
 
 namespace EHR.Roles.Neutral;
--- Roles/Neutral/SchrodingersCat.cs
+++ Roles/Neutral/SchrodingersCat.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EHR.Roles.Impostor
+﻿namespace EHR.Roles.Impostor
 {
     internal class SchrodingersCat : RoleBase
     {
