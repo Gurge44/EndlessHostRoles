72913bfcaeb462cc27bf4d3e798c4bed6f0b6c17
Gurge44 <gurge44.minecraft@gmail.com>
2024-05-16 15:13:41 +0200

    Fix error

--- Modules/CustomTeamManager.cs
+++ Modules/CustomTeamManager.cs
@@ -9,66 +9,11 @@ namespace EHR.Modules
 {
     internal static class CustomTeamManager
     {
-        internal class CustomTeam
-        {
-            public readonly string RoleRevealScreenTitle;
-            public readonly string RoleRevealScreenSubtitle;
-            public readonly string RoleRevealScreenBackgroundColor;
-            public readonly string TeamName;
-
-            public List<CustomRoles> TeamMembers { get; set; } = [];
-
-            public override bool Equals(object obj)
-            {
-                if (obj is not CustomTeam team) return false;
-                return TeamName == team.TeamName;
-            }
-
-            public override int GetHashCode() => TeamName.GetHashCode();
-
-            public CustomTeam(string line)
-            {
-                try
-                {
-                    string[] parts = line.Split(';');
-
-                    TeamName = parts[0];
-                    RoleRevealScreenTitle = parts[1];
-                    RoleRevealScreenSubtitle = parts[2];
-                    RoleRevealScreenBackgroundColor = parts[3];
-
-                    TeamMembers = parts[4].Split(',').Select(x => Enum.Parse<CustomRoles>(x, true)).ToList();
-                }
-                catch (Exception e)
-                {
-                    Utils.ThrowException(e);
-                }
-                finally
-                {
-                    CustomTeams.Add(this);
-                }
-            }
-        }
-
         public static HashSet<CustomTeam> CustomTeams = [];
         public static HashSet<CustomTeam> EnabledCustomTeams = [];
         public static CustomTeam WinnerTeam;
         public static Dictionary<CustomTeam, HashSet<byte>> CustomTeamPlayerIds = [];
         public static List<CustomTeamOptionGroup> CustomTeamOptions = [];
-
-        internal class CustomTeamOptionGroup(CustomTeam team, BooleanOptionItem enabled, BooleanOptionItem knowRoles, BooleanOptionItem winWithOriginalTeam, BooleanOptionItem killEachOther, BooleanOptionItem guessEachOther, BooleanOptionItem arrows)
-        {
-            public CustomTeam Team {get; set;} = team;
-            
-            public BooleanOptionItem Enabled { get; set; } = enabled;
-            public BooleanOptionItem KnowRoles { get; set; } = knowRoles;
-            public BooleanOptionItem WinWithOriginalTeam { get; set; } = winWithOriginalTeam;
-            public BooleanOptionItem KillEachOther { get; set; } = killEachOther;
-            public BooleanOptionItem GuessEachOther { get; set; } = guessEachOther;
-            public BooleanOptionItem Arrows { get; set; } = arrows;
-
-            public List<BooleanOptionItem> AllOptions = [enabled, knowRoles, winWithOriginalTeam, killEachOther, guessEachOther, arrows];
-        }
 
         public static void LoadCustomTeams()
         {
@@ -81,7 +26,7 @@ namespace EHR.Modules
 #pragma warning disable CA1806
                 File.ReadAllLines("./EHR_DATA/CTA_Data.txt").Do(x => new CustomTeam(x));
 #pragma warning restore CA1806
-                
+
                 RefreshCustomOptions();
             }
             catch (Exception e)
@@ -101,7 +46,7 @@ namespace EHR.Modules
             CustomTeamOptions = CustomTeams.Select((x, i) => CreateSetting(x, startId + (6 * i))).ToList();
             OptionSaver.Load();
             EnabledCustomTeams = CustomTeamOptions.Where(x => x.Enabled.GetBool()).Select(x => x.Team).ToHashSet();
-            
+
             return;
 
             static CustomTeamOptionGroup CreateSetting(CustomTeam team, int id)
@@ -183,9 +128,63 @@ namespace EHR.Modules
         {
             var team = GetCustomTeam(id);
             var optionsGroup = CustomTeamOptions.First(x => x.Team.Equals(team));
-            var setting = optionsGroup.GetType().GetFields().FirstOrDefault(x => x.Name == settingName);
+            var setting = optionsGroup.GetType().GetFields().FirstOrDefault(x => x.Name.Contains(settingName));
             if (setting == null) return false;
-            return setting.GetValue(optionsGroup) as bool ?? false;
+            return setting.GetValue(optionsGroup) as bool? ?? false;
+        }
+
+        internal class CustomTeam
+        {
+            public readonly string RoleRevealScreenBackgroundColor;
+            public readonly string RoleRevealScreenSubtitle;
+            public readonly string RoleRevealScreenTitle;
+            public readonly string TeamName;
+
+            public CustomTeam(string line)
+            {
+                try
+                {
+                    string[] parts = line.Split(';');
+
+                    TeamName = parts[0];
+                    RoleRevealScreenTitle = parts[1];
+                    RoleRevealScreenSubtitle = parts[2];
+                    RoleRevealScreenBackgroundColor = parts[3];
+
+                    TeamMembers = parts[4].Split(',').Select(x => Enum.Parse<CustomRoles>(x, true)).ToList();
+                }
+                catch (Exception e)
+                {
+                    Utils.ThrowException(e);
+                }
+                finally
+                {
+                    CustomTeams.Add(this);
+                }
+            }
+
+            public List<CustomRoles> TeamMembers { get; set; } = [];
+
+            public override bool Equals(object obj)
+            {
+                if (obj is not CustomTeam team) return false;
+                return TeamName == team.TeamName;
+            }
+
+            public override int GetHashCode() => TeamName.GetHashCode();
+        }
+
+        internal class CustomTeamOptionGroup(CustomTeam team, BooleanOptionItem enabled, BooleanOptionItem knowRoles, BooleanOptionItem winWithOriginalTeam, BooleanOptionItem killEachOther, BooleanOptionItem guessEachOther, BooleanOptionItem arrows)
+        {
+            public List<BooleanOptionItem> AllOptions = [enabled, knowRoles, winWithOriginalTeam, killEachOther, guessEachOther, arrows];
+            public CustomTeam Team { get; set; } = team;
+
+            public BooleanOptionItem Enabled { get; set; } = enabled;
+            public BooleanOptionItem KnowRoles { get; set; } = knowRoles;
+            public BooleanOptionItem WinWithOriginalTeam { get; set; } = winWithOriginalTeam;
+            public BooleanOptionItem KillEachOther { get; set; } = killEachOther;
+            public BooleanOptionItem GuessEachOther { get; set; } = guessEachOther;
+            public BooleanOptionItem Arrows { get; set; } = arrows;
         }
     }
 }
