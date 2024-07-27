735407eb348e08ac748f77a6d2b3b900e59e379b
Drakos <143632280+Ultradragon005@users.noreply.github.com>
2024-07-27 10:06:25 +0200

    Fix Search Bar bugs, and fix text for Russian (#144)
    * fix
    
    * more russian fix
    
    * bruh
    
    * lol
    
    * move

--- Patches/TextBoxPatch.cs
+++ Patches/TextBoxPatch.cs
@@ -224,6 +224,28 @@ class TextBoxTMPSetTextPatch
     }
 }
 
+
+
+//Thanks https://github.com/NuclearPowered/Reactor/blob/master/Reactor/Patches/Fixes/CursorPosPatch.cs
+
+/// <summary>
+/// "Fixes" an issue where empty TextBoxes have wrong cursor positions.
+/// </summary>
+[HarmonyPatch(typeof(TextMeshProExtensions), nameof(TextMeshProExtensions.CursorPos))]
+internal static class CursorPosPatch
+{
+    public static bool Prefix(TextMeshPro self, ref Vector2 __result)
+    {
+        if (self.textInfo == null || self.textInfo.lineCount == 0 || self.textInfo.lineInfo[0].characterCount <= 0)
+        {
+            __result = self.GetTextInfo(" ").lineInfo[0].lineExtents.max;
+            return false;
+        }
+
+        return true;
+    }
+}
+
 /* Originally by KARPED1EM. Reference: https://github.com/KARPED1EM/TownOfNext/blob/TONX/TONX/Patches/TextBoxPatch.cs */
 [HarmonyPatch(typeof(TextBoxTMP))]
 public class TextBoxPatch
