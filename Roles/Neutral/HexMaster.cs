bd833e359e01604061ac9143175d86fbc5a3a7e2
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-02-04 18:13:59 -0700

    Improve Hex Master

--- Roles/Neutral/HexMaster.cs
+++ Roles/Neutral/HexMaster.cs
@@ -6,7 +6,10 @@ namespace EHR.Neutral;
 public class HexMaster : RoleBase
 {
     private const int Id = 11900;
+    
     public static OptionItem ModeSwitchAction;
+    private static OptionItem CanVent;
+    private static OptionItem HasImpostorVision;
 
     public override bool IsEnable => false;
 
@@ -16,9 +19,25 @@ public class HexMaster : RoleBase
 
         ModeSwitchAction = new StringOptionItem(Id + 10, "WitchModeSwitchAction", Witch.SwitchTriggerText, 2, TabGroup.NeutralRoles)
             .SetParent(CustomRoleSpawnChances[CustomRoles.HexMaster]);
+
+        CanVent = new BooleanOptionItem(Id + 11, "CanVent", true, TabGroup.NeutralRoles)
+            .SetParent(CustomRoleSpawnChances[CustomRoles.HexMaster]);
+
+        HasImpostorVision = new BooleanOptionItem(Id + 12, "ImpostorVision", true, TabGroup.NeutralRoles)
+            .SetParent(CustomRoleSpawnChances[CustomRoles.HexMaster]);
     }
 
     public override void Init() { }
 
     public override void Add(byte playerId) { }
+
+    public override void ApplyGameOptions(IGameOptions opt, byte id)
+    {
+        opt.SetVision(HasImpostorVision.GetBool());
+    }
+
+    public override bool CanUseImpostorVentButton(PlayerControl pc)
+    {
+        return CanVent.GetBool();
+    }
 }
