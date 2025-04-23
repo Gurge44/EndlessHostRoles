31c754965ca4b2a5954dd379bd4501cc1fc336a9
Gurge44 <gurge44.minecraft@gmail.com>
2025-04-23 13:11:37 +0200

    a

--- Main.cs
+++ Main.cs
@@ -37,7 +37,7 @@ public class Main : BasePlugin
     private const string PluginGuid = "com.gurge44.endlesshostroles";
     public const string PluginVersion = "5.5.1";
     public const string PluginDisplayVersion = "5.5.1";
-    public const bool TestBuild = true;
+    public const bool TestBuild = false;
 
     public const string NeutralColor = "#ffab1b";
     public const string ImpostorColor = "#ff1919";
