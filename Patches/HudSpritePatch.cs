46e55f3cbef4552b2d7591dc920beae4f2889b9a
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-04-29 18:44:06 -0700

    Update HudSpritePatch.cs

--- Patches/HudSpritePatch.cs
+++ Patches/HudSpritePatch.cs
@@ -269,6 +269,7 @@ public static class HudSpritePatch
                     break;
                 }
                 case CustomRoles.Agitater:
+                case CustomRoles.Potato:
                 {
                     newKillButton = CustomButton.Get("bombshell");
                     break;
