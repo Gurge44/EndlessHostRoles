bc4e9a9e3ae4900f71b14a01fdb936dedbd80fdb
HyperAtill <hyperatill@gmail.com>
2025-08-04 20:21:35 +0300

    Move Abyssbringer to Impostor Killing category from Misc

--- Modules/CustomRolesHelper.cs
+++ Modules/CustomRolesHelper.cs
@@ -1527,6 +1527,7 @@ internal static class CustomRolesHelper
         return role switch
         {
             CustomRoles.Sans => RoleOptionType.Impostor_Killing,
+            CustomRoles.Abyssbringer => RoleOptionType.Impostor_Killing,
             CustomRoles.Assassin => RoleOptionType.Impostor_Killing,
             CustomRoles.Assumer => RoleOptionType.Impostor_Killing,
             CustomRoles.Augmenter => RoleOptionType.Impostor_Killing,
@@ -1610,7 +1611,6 @@ internal static class CustomRolesHelper
             CustomRoles.Undertaker => RoleOptionType.Impostor_Concealing,
             CustomRoles.Vampire => RoleOptionType.Impostor_Concealing,
             CustomRoles.Warlock => RoleOptionType.Impostor_Concealing,
-            CustomRoles.Abyssbringer => RoleOptionType.Impostor_Miscellaneous,
             CustomRoles.AntiAdminer => RoleOptionType.Impostor_Miscellaneous,
             CustomRoles.Changeling => RoleOptionType.Impostor_Miscellaneous,
             CustomRoles.EvilDiviner => RoleOptionType.Impostor_Miscellaneous,
