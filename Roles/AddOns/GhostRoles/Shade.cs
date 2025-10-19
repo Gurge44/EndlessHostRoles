b3e059e19ec4ca46f8fbee75b680ae0874d78fb6
Gurge44 <gurge44.minecraft@gmail.com>
2025-10-19 10:55:37 +0200

    a

--- Roles/AddOns/GhostRoles/Shade.cs
+++ Roles/AddOns/GhostRoles/Shade.cs
@@ -5,17 +5,22 @@ namespace EHR.AddOns.GhostRoles;
 public class Shade : IGhostRole
 {
     private static OptionItem CD;
+    
     public HashSet<byte> Protected = [];
+    
     public Team Team => Team.Neutral;
     public int Cooldown => CD.GetInt();
+    
     public void OnProtect(PlayerControl pc, PlayerControl target)
     {
         Protected.Add(target.PlayerId);
     }
+    
     public void OnAssign(PlayerControl pc)
     {
         Protected = [];
     }
+    
     public void SetupCustomOption()
     {
         Options.SetupRoleOptions(649900, TabGroup.OtherRoles, CustomRoles.Bloodmoon);
