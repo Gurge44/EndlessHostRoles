829d0f591ff2dc124d9f2c61d49828416717a78c
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-12-20 06:23:12 -0700

    Update RPC.cs

--- Modules/RPC.cs
+++ Modules/RPC.cs
@@ -1661,7 +1661,7 @@ internal static class RPC
                 case Sounds.ImpTransform:
                     SoundManager.Instance.PlaySound(DestroyableSingleton<HnSImpostorScreamSfx>.Instance.HnSOtherImpostorTransformSfx, false, 0.8f);
                     break;
-                case Sounds.YeehawSound:
+                case Sounds.YeehawTransform:
                     SoundManager.Instance.PlaySound(DestroyableSingleton<HnSImpostorScreamSfx>.Instance.HnSOtherYeehawSfx, false, 0.8f);
                     break;
             }
