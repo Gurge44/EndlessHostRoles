4a15b5f219770bfd09bed44c28ea8fcc9f287806
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-12-21 06:54:34 -0700

    Update Utils.cs

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -1395,8 +1395,8 @@ public static class Utils
         }
 
         StringBuilder sb = new("<size=80%>");
-        sb.Append($"{GetRoleName(CustomRoles.GM)}: {(Main.GM.Value ? GetString("RoleRate") : GetString("RoleOff"))}");
-
+        sb.Append($"<color={GetRoleColorCode(CustomRoles.GM)}>{GetRoleName(CustomRoles.GM)}:</color> {(Main.GM.Value ? GetString("RoleRate") : GetString("RoleOff"))}");
+        
         Dictionary<TabGroup, List<string>> roles = new()
         {
             [TabGroup.ImpostorRoles] = [],
