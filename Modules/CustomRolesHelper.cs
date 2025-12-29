77d8b15cf5c36c020f01c0a2b7b6cd160f808072
Wyzeriss <dodobird48482@gmail.com>
2025-12-29 20:01:03 +0000

    Update CustomRolesHelper.cs
    a

--- Modules/CustomRolesHelper.cs
+++ Modules/CustomRolesHelper.cs
@@ -1238,6 +1238,7 @@ internal static class CustomRolesHelper
             CustomRoles.Fool when pc.GetCustomRole() is CustomRoles.Mechanic or CustomRoles.GuardianAngelEHR or CustomRoles.Technician => false,
             CustomRoles.Coroner when pc.Is(CustomRoles.Oblivious) => false,
             CustomRoles.Fragile when pc.Is(CustomRoles.Detour) => false,
+            CustomRoles.Circumvent when pc.Is(CustomRoles.Lurker) => false,
             CustomRoles.DoubleShot when pc.GetCustomRole() is not CustomRoles.EvilGuesser and not CustomRoles.NiceGuesser and not CustomRoles.Augur && !Options.GuesserMode.GetBool() => false,
             CustomRoles.DoubleShot when !pc.Is(CustomRoles.Guesser) && !pc.Is(CustomRoles.EvilGuesser) && pc.Is(CustomRoleTypes.Impostor) && !Options.ImpostorsCanGuess.GetBool() => false,
             CustomRoles.DoubleShot when !pc.Is(CustomRoles.Guesser) && !pc.Is(CustomRoles.NiceGuesser) && pc.Is(CustomRoleTypes.Crewmate) && !Options.CrewmatesCanGuess.GetBool() => false,
@@ -2035,4 +2036,5 @@ public enum CountTypes
 
     Coven
 }
+
 
