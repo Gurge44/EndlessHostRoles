db9ff343d51cac1844cfd53031cac1ef8633c50f
Wyzeriss <dodobird48482@gmail.com>
2025-12-29 20:42:33 +0000

    Update Amogus.cs

--- Roles/Neutral/Amogus.cs
+++ Roles/Neutral/Amogus.cs
@@ -39,7 +39,7 @@ public class Amogus : RoleBase
             .AutoSetupOption(ref SuspiciousSusArrowsToBodies, false)
             .AutoSetupOption(ref UltimateSusVotesPerKill, 1, new IntegerValueRule(0, 10, 0.5), OptionFormat.Votes)
             .AutoSetupOption(ref AbilityCooldown, 20f, new FloatValueRule(0f, 180f, 0.5f), OptionFormat.Seconds)
-            .AutoSetupOption(ref AbilityDuration, 15f, new FloatValueRule(0f, 180f, 0.5f), OptionFormat.Seconds)
+            .AutoSetupOption(ref AbilityDuration, 15f, new FloatValueRule(1f, 180f, 0.5f), OptionFormat.Seconds)
             .AutoSetupOption(ref KillCooldown, 22.5f, new FloatValueRule(0f, 180f, 0.5f), OptionFormat.Seconds)
             .AutoSetupOption(ref CanVent, true)
             .AutoSetupOption(ref ImpostorVision, true);
