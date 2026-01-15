4ba0049bf321f8b39edc6240a126ff5a5f56c767
Gurge44 <gurge44.minecraft@gmail.com>
2026-01-15 11:58:51 +0100

    wait a bit just to make sure

--- Patches/ChatCommandPatch.cs
+++ Patches/ChatCommandPatch.cs
@@ -477,6 +477,7 @@ internal static class ChatCommands
             {
                 WaitingToSend = true;
                 while (Utils.TempReviveHostRunning && AmongUsClient.Instance.AmHost) yield return null;
+                yield return new WaitForSeconds(0.5f);
                 if (GameStates.IsEnded || GameStates.IsLobby) yield break;
                 if (HudManager.InstanceExists) HudManager.Instance.Chat.SendChat();
                 WaitingToSend = false;
