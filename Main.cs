246a6f1bb5a0478e826bf4a5fa3c0b6a7c4d87b3
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-04-02 12:18:01 -0400

    modded handshake (needs test)

--- Main.cs
+++ Main.cs
@@ -40,7 +40,7 @@ public class Main : BasePlugin
 {
     private const string DebugKeyHash = "c0fd562955ba56af3ae20d7ec9e64c664f0facecef4b3e366e109306adeae29d";
     private const string DebugKeySalt = "59687b";
-    private const string PluginGuid = "com.gurge44.endlesshostroles";
+    public const string PluginGuid = "com.gurge44.endlesshostroles";
     public const string PluginVersion = "7.4.0";
     public const string PluginDisplayVersion = "7.4.0";
     public const bool TestBuild = false;
