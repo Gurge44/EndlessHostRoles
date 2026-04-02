391d225fc77b9c7a30d48c68890084b322dcb3ef
Tommy XL <fadeevvladimir16@gmail.com>
2026-04-02 20:51:01 +0800

    Remove TeamSpawnOpt for Torch

--- Roles/Standard/Addons/Common/Torch.cs
+++ Roles/Standard/Addons/Common/Torch.cs
@@ -8,7 +8,7 @@ internal class Torch : IAddon
 
     public void SetupCustomOption()
     {
-        SetupAdtRoleOptions(14200, CustomRoles.Torch, canSetNum: true, teamSpawnOptions: true);
+        SetupAdtRoleOptions(14200, CustomRoles.Torch, canSetNum: true);
 
         TorchVision = new FloatOptionItem(14210, "TorchVision", new(0.25f, 5f, 0.05f), 1.25f, TabGroup.Addons)
             .SetParent(CustomRoleSpawnChances[CustomRoles.Torch])
