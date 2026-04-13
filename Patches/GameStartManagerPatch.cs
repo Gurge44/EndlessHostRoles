6b105e60940bc56ee3de8ff822f08b58d18ac922
New78799 <newholiday787@gmail.com>
2026-04-13 22:17:04 +0700

    Update GameStartManagerPatch.cs

--- Patches/GameStartManagerPatch.cs
+++ Patches/GameStartManagerPatch.cs
@@ -111,8 +111,8 @@ public static class GameStartManagerPatch
                 AURoleOptions.SetOpt(Main.NormalOptions.CastFast<IGameOptions>());
                 if (AURoleOptions.ShapeshifterCooldown == 0f) AURoleOptions.ShapeshifterCooldown = Main.LastShapeshifterCooldown.Value;
 
-                AURoleOptions.GuardianAngelCooldown = Spiritcaller.SpiritAbilityCooldown.GetFloat();
-                AURoleOptions.ProtectionDurationSeconds = 0f;
+                //AURoleOptions.GuardianAngelCooldown = Spiritcaller.SpiritAbilityCooldown.GetFloat();
+                //AURoleOptions.ProtectionDurationSeconds = 0f;
             }
             catch (Exception ex) { Logger.Error(ex.ToString(), "GameStartManagerStartPatch.Postfix (1)"); }
         }
