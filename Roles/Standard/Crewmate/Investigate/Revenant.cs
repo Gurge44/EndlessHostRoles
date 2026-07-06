2c6f4075d4879afda7b58e9d909a34f6085fc6b6
Gurge44 <gurge44.minecraft@gmail.com>
2026-07-06 19:33:42 +0200

    fix & clean up revenant code

--- Roles/Standard/Crewmate/Investigate/Revenant.cs
+++ Roles/Standard/Crewmate/Investigate/Revenant.cs
@@ -1,16 +1,9 @@
-﻿using EHR.Modules;
-using Hazel;
-
-namespace EHR.Roles;
-
-using static Options;
-using static Utils;
+﻿namespace EHR.Roles;
 
 internal class Revenant : RoleBase
 {
     public static bool On;
     public static OptionItem KnowInfo;
-    private static OptionItem RemainingTasksToBeFound;
 
     private static readonly string[] KnowInfoMode =
     [
@@ -18,25 +11,16 @@ internal class Revenant : RoleBase
         "Roles"
     ];
 
-    public bool TaskDone;
-    private bool StillAlive;
-    private bool IsExposed;
     private byte RevenantId;
+    private bool ShouldBeRevived;
 
     public override bool IsEnable => On;
 
     public override void SetupCustomOption()
     {
-        int id = 659500;
-        SetupRoleOptions(id++, TabGroup.CrewmateRoles, CustomRoles.Revenant);
-
-        KnowInfo = new StringOptionItem(id++, "Revenant.KnowInfo", KnowInfoMode, 1, TabGroup.CrewmateRoles)
-            .SetParent(CustomRoleSpawnChances[CustomRoles.Revenant]);
-
-        RemainingTasksToBeFound = new IntegerOptionItem(id++, "SnitchRemainingTaskFound", new(0, 10, 1), 1, TabGroup.CrewmateRoles)
-            .SetParent(CustomRoleSpawnChances[CustomRoles.Revenant]);
-
-        Options.OverrideTasksData.Create(id++, TabGroup.CrewmateRoles, CustomRoles.Revenant);
+        StartSetup(659100)
+            .AutoSetupOption(ref KnowInfo, 1, KnowInfoMode)
+            .CreateOverrideTasksData();
     }
 
     public override void Init()
@@ -47,71 +31,39 @@ internal class Revenant : RoleBase
     public override void Add(byte playerId)
     {
         On = true;
-        TaskDone = false;
-        StillAlive = false;
-        IsExposed = false;
         RevenantId = playerId;
+        ShouldBeRevived = false;
     }
 
     public override void OnReportDeadBody()
     {
-        if (!TaskDone && RevenantId.GetPlayer().IsAlive())
+        PlayerControl pc = RevenantId.GetPlayer();
+
+        if (pc && pc.IsAlive() && !pc.AllTasksCompleted())
         {
+            ShouldBeRevived = true;
+            pc.RpcExiled();
             PlayerState state = Main.PlayerStates[RevenantId];
             state.deathReason = PlayerState.DeathReason.Suicide;
             state.SetDead();
-            StillAlive = true;
-            SendRPC(CustomRPC.SyncRoleData, RevenantId, TaskDone, StillAlive, IsExposed);
         }
     }
 
     public override void AfterMeetingTasks()
     {
+        if (!ShouldBeRevived) return;
+        
         LateTask.New(() =>
         {
-            if (!GameStates.IsInTask || ExileController.Instance || AntiBlackout.SkipTasks) return;
-            if (!Main.PlayerStates.TryGetValue(RevenantId, out var state)) return;
+            if (GameStates.IsEnded || !GameStates.IsInTask || ExileController.Instance || AntiBlackout.SkipTasks) return;
 
             PlayerControl pc = RevenantId.GetPlayer();
 
-            if (pc && StillAlive)
+            if (pc)
             {
-                RPC.PlaySoundRPC(RevenantId, Sounds.SpawnSound);
-                GhostRolesManager.RemoveGhostRole(RevenantId);
                 pc.RpcRevive();
                 pc.TPToRandomVent();
-                StillAlive = false;
-                SendRPC(CustomRPC.SyncRoleData, RevenantId, TaskDone, StillAlive, IsExposed);
             }
-        }, 2f, "Revive Delay");
-    }
-
-    public override void OnTaskComplete(PlayerControl pc, int completedTaskCount, int totalTaskCount)
-    {
-        if (!pc.IsAlive() && !GameStates.IsMeeting) return;
-
-        if (totalTaskCount - (completedTaskCount + 1) <= RemainingTasksToBeFound.GetInt() && !IsExposed)
-        {
-            foreach (PlayerControl target in Main.CachedAlivePlayerControls())
-            {
-                TargetArrow.Add(target.PlayerId, pc.PlayerId);
-                NotifyRoles(SpecifySeer: target, SpecifyTarget: target);
-            }
-            IsExposed = true;
-        }
-
-        if (completedTaskCount + 1 >= totalTaskCount)
-        {
-            TaskDone = true;
-            pc.Notify(Translator.GetString("RevenantDoneTasks"));
-        }
-        SendRPC(CustomRPC.SyncRoleData, RevenantId, TaskDone, StillAlive, IsExposed);
-    }
-
-    public void ReceiveRPC(MessageReader reader)
-    {
-        TaskDone = reader.ReadBoolean();
-        StillAlive = reader.ReadBoolean();
-        IsExposed = reader.ReadBoolean();
+        }, 2f, "Revenant Revive Delay");
     }
 }
